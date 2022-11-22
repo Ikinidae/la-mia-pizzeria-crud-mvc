1d89c2f71ac3419684c988215d259579a3fe7e96
feder <federicaelia24@gmail.com>
2022-11-22 15:36:41 +0100

    Aggiungere i file di progetto.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,63 @@
+﻿using la_mia_pizzeria_static.Data;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        PizzeriaDbContext db;
+
+        public PizzaController() : base()
+        {
+            db = new PizzeriaDbContext();
+        }
+
+        public IActionResult Index()
+        {
+            List<Pizza> listPizzas = db.Pizzas.ToList();
+
+            return View(listPizzas);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Pizza pizza = db.Pizzas.Where(p => p.Id == id).FirstOrDefault();
+
+            return View(pizza);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Pizza pizza)
+        {
+            if (!ModelState.IsValid)
+            {
+                //return View(post);
+                return View();
+            }
+
+            db.Pizzas.Add(pizza);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        //public IActionResult Update(int id)
+        //{
+        //    Post post = db.Posts.Where(post => post.Id == id).FirstOrDefault();
+
+        //    if (post == null)
+        //        return NotFound();
+
+        //    //return View() --> non funziona perchè non ha la memoria della post
+        //    return View(post);
+        //}
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,43 @@
+﻿using la_mia_pizzeria_static.Validator;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        //properties
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [StringLength(100, ErrorMessage = "Il titolo non può essere più lungo di 100 caratteri")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [StringLength(100, ErrorMessage = "La descrizione non può essere più lunga di 500 caratteri")]
+        [Column(TypeName = "text")]
+        [FiveWords]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        public string Image { get; set; }
+
+        public float Price { get; set; }
+
+
+        //costruttore
+        public Pizza (string name, string description, string image, float price)
+        {
+            Name= name;
+            Description= description;
+            Image= image;
+            Price= price;
+        }
+
+        public Pizza ()
+        {
+
+        }
+    }
+}
